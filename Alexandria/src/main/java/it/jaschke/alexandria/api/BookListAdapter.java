d4691c93e80ce195a908c7945668b45a221fa69d
Johann Reyes <jreyes@vaporwarecorp.com>
2015-08-08 14:23:53 -0400

    Adding Picasso to the BookListAdapter

--- Alexandria/src/main/java/it/jaschke/alexandria/api/BookListAdapter.java
+++ Alexandria/src/main/java/it/jaschke/alexandria/api/BookListAdapter.java
@@ -1,6 +1,5 @@
 package it.jaschke.alexandria.api;
 
-
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
@@ -10,39 +9,27 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import it.jaschke.alexandria.R;
 import it.jaschke.alexandria.data.AlexandriaContract;
 import it.jaschke.alexandria.services.DownloadImage;
 
-/**
- * Created by saj on 11/01/15.
- */
 public class BookListAdapter extends CursorAdapter {
-
-
-    public static class ViewHolder {
-        public final ImageView bookCover;
-        public final TextView bookTitle;
-        public final TextView bookSubTitle;
-
-        public ViewHolder(View view) {
-            bookCover = (ImageView) view.findViewById(R.id.fullBookCover);
-            bookTitle = (TextView) view.findViewById(R.id.listBookTitle);
-            bookSubTitle = (TextView) view.findViewById(R.id.listBookSubTitle);
-        }
-    }
+// --------------------------- CONSTRUCTORS ---------------------------
 
     public BookListAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
+// -------------------------- OTHER METHODS --------------------------
+
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-
         ViewHolder viewHolder = (ViewHolder) view.getTag();
 
         String imgUrl = cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
-        new DownloadImage(viewHolder.bookCover).execute(imgUrl);
+        Picasso.with(context).load(imgUrl).error(R.drawable.ic_launcher).into(viewHolder.bookCover);
 
         String bookTitle = cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
         viewHolder.bookTitle.setText(bookTitle);
@@ -54,10 +41,21 @@ public class BookListAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         View view = LayoutInflater.from(context).inflate(R.layout.book_list_item, parent, false);
-
-        ViewHolder viewHolder = new ViewHolder(view);
-        view.setTag(viewHolder);
-
+        view.setTag(new ViewHolder(view));
         return view;
+    }
+
+// -------------------------- INNER CLASSES --------------------------
+
+    public static class ViewHolder {
+        public final ImageView bookCover;
+        public final TextView bookTitle;
+        public final TextView bookSubTitle;
+
+        public ViewHolder(View view) {
+            bookCover = (ImageView) view.findViewById(R.id.fullBookCover);
+            bookTitle = (TextView) view.findViewById(R.id.listBookTitle);
+            bookSubTitle = (TextView) view.findViewById(R.id.listBookSubTitle);
+        }
     }
 }
